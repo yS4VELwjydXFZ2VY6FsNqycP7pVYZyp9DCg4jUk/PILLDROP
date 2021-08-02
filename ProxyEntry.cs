7d5e9bbd3e6ab37b0f20cd43ecdd01d79572030a
riya <54314240+riya69@users.noreply.github.com>
2021-08-02 23:30:47 +0200

    Create ProxyEntry.cs

---
+++ ProxyEntry.cs
@@ -0,0 +1,11 @@
+namespace PILLDROP
+{
+    /// <summary>
+    /// Basic proxy structure.
+    /// </summary>
+    internal struct ProxyEntry
+    {
+        public string ip;
+        public ushort port;
+    }
+}
